4bea90b3f85309e38e873382fcb2ea99374f19b6
David Burbano <odga95@gmail.com>
2024-07-24 22:57:00 -0500

    files

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import '../sass/app.scss';
+import { createApp } from 'vue';
+import ExampleComponent from './components/ExampleComponent.vue';
+
+const app = createApp({});
+app.component('example-component', ExampleComponent);
+app.mount('#app');
